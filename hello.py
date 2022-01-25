07476af649ce77fe3df0d45ee9ad9d9c938fbb4f
kyabe314 <97861373+kyabe314@users.noreply.github.com>
2022-01-25 16:39:57 -0500

    class notes 1/25

--- hello.py
+++ hello.py
@@ -1,1 +1,39 @@
-print('Hello, world!')
+#name = input('Please enter your name>> ')
+
+#print('Hello',name)
+#ctrl + f5
+#input function = asks user to enter something
+#assign a string to the variable from the input of users
+
+# equal sign is an assignment operator
+# id()
+
+#print('hello\n'*10)
+#\n = end of a line
+
+#print(f'Hello, {name}')
+
+#pi = 3.1415926
+
+#print(f'Pi equals {pi:.5f}.')  #f is floating point
+#print(f'Pi equals {pi:8.5f}.') #8 in total, 5 after decimal
+#print(f'Pi equals {pi:8.2f}.') #8 in total, 2 after decimal
+
+#a = 2021
+
+#binary
+#print(f'{a:x}')
+
+#hexadecimal
+#print(f'{a:x}')
+
+#octal
+#print(f'{a:o}')
+
+# scientific
+# print(f'{a:e}')
+
+# We finished session 3!
+
+# ctrl + / = block comment in/out
+# use f-string
---
+++ session02/hello.py
@@ -0,0 +1,10 @@
+print('Hello, world!')
+
+print(2 + -2)
+# alt + shift + F = format
+
+print(13 % 4)
+#gives us remainder
+
+print(divmod(13, 4))
+#returns(quotient, remainder)
