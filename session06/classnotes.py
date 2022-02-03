3c193d200fcfc6864b18560d2f9d5788adcad5f7
kyabe314 <97861373+kyabe314@users.noreply.github.com>
2022-02-03 16:30:44 -0500

    Class notes Session 05

---
+++ session06/classnotes.py
@@ -0,0 +1,68 @@
+# Tue/Thu 6:30-7:00 office hours
+# always add docstrings, followed by comments
+# function variables and parameters are local
+
+# def f(x):
+#     y = x + 1
+#     return y
+
+# z = f(5)
+# print(z)
+
+# If a function does not return any value, it is NONE
+# return is an assignment to a value
+# print() is for debugging purpose
+# y is a local variable and hence does not exist
+
+# Return immediately terminates the fucntion
+# Put some valid value in and a function which does not take any value will run
+# Add comments for what to do / pass / return
+
+# def f():
+#     print('Hi')
+
+# print(f())
+
+# first, f() was called within the print function
+# there is no value assigned to f(), so nothing will be printed
+# It is a function, not a variable
+
+# if f(a), is if f(a) is not None
+
+# Add more comments!!!
+
+# Docstrngs, Docstrings, Docstrings!!!
+
+# What if exercises are too difficult?
+#
+# 1. Do I understand the concepts?
+# 2. Have I attempted example/demo code?
+# 3. Have I read supplemental materials?
+# 4. Have I tried any debugging approaches?
+# 5. Have I had enough rest?
+# 6. Have I asked for help?
+#   i. internal community (Slack/GitHub/Email)
+#   ii. external community
+#       -Please give credits for use of code  DO IT!
+#       -Cite URLs as references in comments
+
+# raise TypeError("Customized Error Message")
+
+# Pseudo-code = using normal English to decribe codes
+
+# DOCSTRINGS!!!
+
+# Keep a function single-purposed!!!
+
+# if x = TRUE: >>> if x:
+
+# print(2 ** 38)
+
+# If I wanna learn python, I have to code a lot!!!
+
+# Interface design
+# the turtle module
+
+
+
+
---
+++ session06/my_polygon.py
@@ -0,0 +1,45 @@
+import turtle
+
+leo = turtle.Turtle()
+print(leo)
+
+# leo.fd(100)
+# leo.lt(90)
+# leo.fd(100)
+# leo.lt(90)
+# leo.fd(100)
+# leo.lt(90)
+# leo.fd(100)
+# leo.lt(90)
+
+
+
+def square(t):
+    """ Write a function that takes parameter t, which is a turle. It should use the turtle to draw a square.
+    """
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+    
+
+
+# square(leo)
+
+# raphael = turtle.Turtle()
+# square(raphael)
+
+def square(t, length):
+    """this function takes a parameter named t, which is a turtle, and another parameter length, which is a float. It should use the turtle to draw a swuare with side length, length"""
+
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+        # generalization
+
+square(leo, 150)
+
+turtle.mainloop()
+
+# when told pause, pause!!!
