48391bb9988ede9620eccad2936a28ea9cfd7623
kyabe314 <97861373+kyabe314@users.noreply.github.com>
2022-04-07 21:09:56 -0400

    Class materials

---
+++ session20/classnotes_20.py
@@ -0,0 +1,16 @@
+# timeless debugging
+# Analyze the data first
+# cloud -> uses a part of the server
+
+# Web scrapting
+# the process of automatically collecting web data with specialized software
+
+# lead generation, price monitoring
+# visible on chrome -> scrapable
+
+# repsponse
+# 200 -> ok
+# 404 -> URL not found
+# 500 internal server error
+
+# HTML -> tree structure
